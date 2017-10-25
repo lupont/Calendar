2bd466112a23b4d6ab22adc4d1d1007ae539235e
lupont <laos.pontus@gmail.com>
2017-10-25 14:53:38 +0200

    Added solution and basic functionality.

---
+++ Calendar/Calendar/Calendar/IndividualDayPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Calendar
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class IndividualDayPage : ContentPage
+    {
+        private Label _label;
+
+        public IndividualDayPage()
+        {
+            InitializeComponent();
+            _label = this.FindByName<Label>("LblDateAndTime");
+        }
+
+        public IndividualDayPage(DateTime date, TimeSpan time) : this()
+        {
+            var d = date.Date.ToString("yyyy-MM-dd");
+            var t = time.ToString(@"hh\:mm");
+            var dateAndTime = $"{d}: {t}";
+
+            _label.Text = dateAndTime;
+        }
+    }
+}
---
+++ Calendar/Calendar/Calendar/MainPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.ComponentModel;
+
+namespace Calendar
+{
+    public partial class MainPage : ContentPage
+    {
+        #region Private fields
+        private DatePicker _datePicker;
+        private Label _labelDate, _labelTime;
+        private StackLayout _stackLayoutHours;
+        #endregion
+
+        #region Constructors
+        public MainPage()
+        {
+            InitializeComponent();
+            _datePicker       = this.FindByName<DatePicker>("DprDate");
+            _labelDate        = this.FindByName<Label>("LblDate");
+            _labelTime        = this.FindByName<Label>("LblTime");
+            _stackLayoutHours = this.FindByName<StackLayout>("StlHours");
+
+            AddHourButtons();
+        }
+        #endregion
+
+        #region Methods
+        private void AddDays(int amountOfDaysToAdd)
+        {
+            _datePicker.Date += TimeSpan.FromDays(amountOfDaysToAdd);
+        }
+
+        private void AddHourButtons()
+        {
+            if (_stackLayoutHours.Children.Any(child => child is Button))
+                return;
+
+            for (int i = 0; i < 24; i++)
+            {
+                var button = new Button()
+                {
+                    Text = TimeSpan.FromHours(i).ToString(@"hh\:mm"),
+                }; 
+                button.Clicked += SwitchToIndividualDay;
+                _stackLayoutHours.Children.Add(button);
+            }
+        }
+        #endregion
+
+        #region Event handlers
+        private void BtnPreviousDay_Clicked(object sender, EventArgs e)
+        {
+            AddDays(-1);
+        }
+
+        private void BtnNextDay_Clicked(object sender, EventArgs e)
+        {
+            AddDays(1);
+        }
+
+        private void HandleDateChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName != DatePicker.DateProperty.PropertyName)
+                return;
+
+            AddHourButtons();
+        }
+
+        private async void SwitchToIndividualDay(object sender, EventArgs e)
+        {
+            if (!(sender is Button button))
+                return;
+
+            await Navigation.PushAsync(new IndividualDayPage(_datePicker.Date, TimeSpan.Parse(button.Text)));
+        }
+        #endregion
+    }
+}
